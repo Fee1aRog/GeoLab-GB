aff0cbd0740f89ab8a1d89ffae90652b13d847fd
George <barishvili2001@gmail.com>
2021-12-02 15:41:55 +0400

    hw1

--- Homework1/hw1.js
+++ Homework1/hw1.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const cars = ['Mercedes','BMW','Audi','Porsche'];
+const iterator1 = cars.keys();
+
+for (const key of iterator1) {
+    console.log(key);
+  }
+  //ლოგავს ელემენტების ინდექსებს და შესაბამისად მათ რაოდენობას თუ რამდენი ელემნტია მასივში.
+  
+const iterator2 = cars.values();
+
+for (const value of iterator2) {
+  console.log(value);
+}
+//ლოგავს ელემენტებს ინდექსის მიხედვით
+cars.sort();
+console.log(cars);
+//ლოგავს ანბანის მიხედვით სორტირებულ ელემენტებს
